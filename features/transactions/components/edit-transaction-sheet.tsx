a7be619f51d9d5a0416921327f3f4360763bea21
sadeq <sadeghhashemi7575@gmail.com>
2024-10-18 11:20:20 +0330

    Update edit-transaction-sheet.tsx

--- features/transactions/components/edit-transaction-sheet.tsx
+++ features/transactions/components/edit-transaction-sheet.tsx
@@ -6,7 +6,7 @@ import {
   SheetTitle,
 } from "@/components/ui/sheet";
 
-import { insertAccountsSchema } from "@/db/schema";
+import { insertTransActionSchema } from "@/db/schema";
 import { z } from "zod";
 import { Loader2 } from "lucide-react";
 import { useConfirm } from "@/hooks/use-confirm";
@@ -14,15 +14,16 @@ import { useGetTransaction } from "../api/use-get-transaction";
 import { useEditTransaction } from "../api/use-edit-transaction";
 import { useDeleteTransaction } from "../api/use-delete-transaction";
 import { useOpenTransaction } from "../hooks/use-open-transaction";
-const formSchema = insertAccountsSchema.pick({
-  name: true,
+import { TransactionForm } from "./transaction-form";
+const formSchema = insertTransActionSchema.omit({
+  id: true,
 });
 type FormValues = z.input<typeof formSchema>;
 export const EditTransactionSheet = () => {
   const { isOpen, onClose, id } = useOpenTransaction();
   const [ConfirmDialog, confirm] = useConfirm(
     "are you sure?",
-    "You are about to delete this account"
+    "You are about to delete this transaction"
   );
   const transactionQuery = useGetTransaction(id);
   const editMutation = useEditTransaction(id);
@@ -55,27 +56,31 @@ export const EditTransactionSheet = () => {
           ? new Date(transactionQuery.data.date)
           : new Date(),
         payee: transactionQuery.data.payee,
+        notes: transactionQuery.data.notes,
       }
-    : { name: "" };
+    : {
+        acountId: "",
+        categoryId: "",
+        amount: "",
+        date: "",
+        payee: "",
+        notes: "",
+      };
   return (
     <>
       <ConfirmDialog />
       <Sheet open={isOpen} onOpenChange={onClose}>
         <SheetContent className="space-y-4">
           <SheetHeader>
-            <SheetTitle>{id ? "Edit Account" : "New Account"}</SheetTitle>
-            <SheetDescription>
-              {id
-                ? "Edit the account details."
-                : "Create a new account to track your transactions."}
-            </SheetDescription>
+            <SheetTitle>Edit Transaction</SheetTitle>
+            <SheetDescription>Edit an existing transaction</SheetDescription>
           </SheetHeader>
           {isLoading ? (
             <div className="absolute inset-0 flex items-center justify-center">
               <Loader2 className="size-4 text-muted-foreground animate-spin" />
             </div>
           ) : (
-            <AccountForm
+            <TransactionForm
               onSubmit={onSubmit}
               disabled={isPending}
               defaultValues={defaultValues}
