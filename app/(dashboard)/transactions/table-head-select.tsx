8dcbea8f48d00a2aceba4f574b9adab9da09803a
sadeq <sadeghhashemi7575@gmail.com>
2024-10-21 11:41:38 +0330

    Update table-head-select.tsx

--- app/(dashboard)/transactions/table-head-select.tsx
+++ app/(dashboard)/transactions/table-head-select.tsx
@@ -1,3 +1,8 @@
+import { Select } from "@/components/select";
+import { SelectContent, SelectItem, SelectValue } from "@/components/ui/select";
+import { cn } from "@/lib/utils";
+import { SelectTrigger } from "@radix-ui/react-select";
+
 type Props = {
   columnIndex: number;
   selectedColumns: Record<string, string | null>;
@@ -10,5 +15,38 @@ export const TableHeadSelect = ({
   selectedColumns,
   onChange,
 }: Props) => {
-  return <div>Select</div>;
+  const currentSelection = selectedColumns[`column_${columnIndex}`];
+  return (
+    <Select
+      value={currentSelection || ""}
+      onValueChange={(value) => onChange(columnIndex, value)}
+    >
+      <SelectTrigger
+        className={cn(
+          "focus:ring-offset-0 focus:ring-transparent outline-none border-none  bg-transparent capitalize",
+          currentSelection && "text-blue-500"
+        )}
+      >
+        <SelectValue placeholder="Skip" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="skip">Skip</SelectItem>
+        {options.map((option, index) => {
+          const disabled =
+            Object.values(selectedColumns).includes(option) &&
+            selectedColumns[`column_${columnIndex}`] !== option;
+          return (
+            <SelectItem
+              key={index}
+              value={option}
+              disabled={disabled}
+              className="capitalize"
+            >
+              {option}
+            </SelectItem>
+          );
+        })}
+      </SelectContent>
+    </Select>
+  );
 };
