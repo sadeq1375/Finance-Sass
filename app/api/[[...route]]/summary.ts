4cb50e13616fe76009a923c164d89a54555d76e3
sadeq <sadeghhashemi7575@gmail.com>
2024-10-25 15:59:31 +0330

    Update summary.ts

--- app/api/[[...route]]/summary.ts
+++ app/api/[[...route]]/summary.ts
@@ -1,8 +1,11 @@
 import { zValidator } from "@hono/zod-validator";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { Hono } from "hono";
-import { z } from "zod";
+import { number, z } from "zod";
 import { differenceInDays, parse, subDays } from "date-fns";
+import { db } from "@/db/drizzle";
+import { and, eq, gte, lte, sql, sum } from "drizzle-orm";
+import { accounts, transactions } from "@/db/schema";
 
 const app = new Hono().get(
   "/",
@@ -18,9 +21,12 @@ const app = new Hono().get(
   async (c) => {
     const auth = getAuth(c);
     const { from, to, accountId } = c.req.valid("query");
-    if (auth?.userId) {
+
+    // Check if userId is missing, return unauthorized if true
+    if (!auth?.userId) {
       return c.json({ error: "Unauthorized" }, 401);
     }
+
     const defaultTo = new Date();
     const defaultFrom = subDays(defaultTo, 30);
     const startDate = from
@@ -30,6 +36,49 @@ const app = new Hono().get(
     const periodLength = differenceInDays(endDate, startDate) + 1;
     const lastPeroidStart = subDays(startDate, periodLength);
     const lastPeroidEnd = subDays(endDate, periodLength);
+
+    async function fetchFinancialData(
+      userId: string,
+      startDate: Date,
+      endDate: Date
+    ) {
+      return db
+        .select({
+          income:
+            sql`SUM(CASE WHEN ${transactions.amount} >= 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(
+              number
+            ),
+          expenses:
+            sql`SUM(CASE WHEN ${transactions.amount} < 0 THEN ${transactions.amount} ELSE 0 END)`.mapWith(
+              number
+            ),
+          remaining: sum(transactions.amount).mapWith(number),
+        })
+        .from(transactions)
+        .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+        .where(
+          and(
+            accountId ? eq(transactions.accountId, accountId) : undefined,
+            eq(accounts.userId, userId),
+            gte(transactions.date, startDate),
+            lte(transactions.date, endDate)
+          )
+        );
+    }
+
+    // Use auth.userId with non-null assertion
+    const [currentPeriod] = await fetchFinancialData(
+      auth.userId!,
+      startDate,
+      endDate
+    );
+    const [lastPeriod] = await fetchFinancialData(
+      auth.userId!,
+      lastPeroidStart,
+      lastPeroidEnd
+    );
+
+    return c.json({ currentPeriod, lastPeriod });
   }
 );
 
